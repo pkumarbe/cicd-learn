bbdf88d389d450fa1d6389e83256f651dc7464c2
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-11 19:52:29 +0000

    corrected secret

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to gcp World Computeeee"
+    return "Welcome to gcp World Compute"
 
 if __name__ == "__main__":
 
