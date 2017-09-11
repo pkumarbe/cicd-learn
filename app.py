d2b7d56212892ff79d97dd1c1dd02098027f1b24
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-11 20:34:30 +0000

    edited app

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Welcome to CICD"
-@app.route("/hello")
-def hello():
-    return "Hello Mr "
+@app.route("/gcp")
+def gcp():
+    return "welcome to GCP"
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'))
