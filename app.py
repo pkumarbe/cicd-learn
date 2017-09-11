2d5fbbb43c74e459e082342a9a5b79d9ed96c9fc
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-11 20:14:27 +0000

    remove image everytime

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Welcome to gcp World Compute"
-
+@app.route("/hello")
+def hello():
+    return "Hello Mr "
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'))
