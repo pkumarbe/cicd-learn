e968aa7a4718896880682d3718508a866a1d008c
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-12 14:16:35 +0000

    restart

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome to CICD demo"
+    return "Welcome to CICD demoi online"
 @app.route("/gcp")
 def gcp():
     return "welcome to GCP"
