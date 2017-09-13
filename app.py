2256b763914ffbb9e6d95616beeb1105dde5fad3
root <root@bastion-host.c.intrepid-tape-166716.internal>
2017-09-13 15:09:11 +0000

    drone server testing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     return "Welcome to CICD demo online test"
 @app.route("/gcp")
 def gcp():
-    return "welcome to GCP"
+    return "welcome to GCP, Amazing"
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=int('8001'))
